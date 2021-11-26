bd7554df23b74228bb030a8ed647ad54045aac13
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2021-11-26 12:40:52 +0100

    init project

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid bg">
+      <div class="row">
+          <div class="col p-2">
+                <img src="../assets/img/logo.png" alt="">
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style lang="scss">
+    .bg{
+        background-color:#2e3a46;
+        height: 100px;
+
+        img{
+            width: 260px;
+        }
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container-fluid bg">
+      <div class="container">
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Main"
+}
+</script>
+
+<style lang="scss" scoped>
+    .bg{
+        background-color: #1e2d3b;
+        min-height: calc(100vh - 100px);
+    }
+</style>
